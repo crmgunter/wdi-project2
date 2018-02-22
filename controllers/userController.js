7cf19c63fc350d7c59d5d34be89a88d8f5e9d080
crmgunter <crmgunter@gmail.com>
2018-02-21 20:50:15 -0500

    added /new page and /new get route, created post route and info is being stored in database when new user created and that info is going to users index.

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,12 +3,39 @@ const router = express.Router();
 const User = require('../models/user')
 
 /* GET users listing. */
-router.get('/', function(req, res) {
+router.get('/', function (req, res) {
   User.find().then((users) => {
     res.render('user/index', {
       users: users
     })
   })
 });
+
+router.get('/new', (req, res) => {
+  res.render('user/new')
+})
+
+
+router.post('/', (req, res) => {
+  const newUser = new User({
+    username: req.body.username,
+    image: req.body.image,
+    email: req.body.email,
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    phoneNumber: req.body.phoneNumber
+  })
+  newUser.save().then((savedUser) => {
+    res.redirect(`/users/${savedUser._id}`)
+  })
+})
+
+router.get(':/id', (req, res) => {
+  User.findById(req.params.id).then((user) => {
+    res.render('user/show', {
+      user: user
+    })
+  })
+})
 
 module.exports = router;
