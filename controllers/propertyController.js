51d937b289394993fa1186613c3af79a279977fd
crmgunter <crmgunter@gmail.com>
2018-02-22 17:25:57 -0500

    added show route and back to all properties button

--- controllers/propertyController.js
+++ controllers/propertyController.js
@@ -1,48 +1,60 @@
 const express = require('express');
-const router = express.Router({mergeParams: true});
+const router = express.Router({ mergeParams: true });
+
 const User = require('../models/user')
 const Property = require('../models/property')
 
 /* GET users listing. */
 router.get('/', function (req, res) {
-  User.findById(req.params.userId).then((user) => {
-    console.log(user)
-    const properties = user.properties
-    res.render('property/index', {
-      user: user,
-      properties: properties
+    User.findById(req.params.userId).then((user) => {
+        console.log(user)
+        const properties = user.properties
+        res.render('property/index', {
+            user: user,
+            properties: properties
+        })
     })
-  })
 });
 
 // NEW
 router.get('/new', (req, res) => {
-  res.render('property/new')
+    res.render('property/new', {
+        userId: req.params.userId
+    })
 })
 
-// // POST 
-// router.post('/', (req, res) => {
-//   const newUser = new User({
-//     username: req.body.username,
-//     image: req.body.image,
-//     email: req.body.email,
-//     firstName: req.body.firstName,
-//     lastName: req.body.lastName,
-//     phoneNumber: req.body.phoneNumber
-//   })
-//   newUser.save().then((savedUser) => {
-//     res.redirect(`/users/${savedUser._id}`)
-//   })
-// })
+// POST 
+router.post('/', (req, res) => {
+    const userId = req.params.userId
+    const newProperty = req.body
 
-// // SHOW PAGE
-// router.get('/:id', (req, res) => {
-//   User.findById(req.params.id).then((user) => {
-//     res.render('user/show', {
-//       user: user
-//     })
-//   })
-// })
+    User.findById(userId).then((user) => {
+        const newProperty = new Property({
+            address: req.body.address,
+            landlord: req.body.landlord,
+            price: req.body.price,
+            description: req.body.description,
+            homeImage: req.body.homeImage
+        })
+        
+        user.properties.push(newProperty)
+        return user.save()
+    }).then((updatedUser) => {
+        res.redirect(`/users/${req.params.userId}/properties`)
+    })
+})
+
+// SHOW PAGE
+router.get('/:id', (req, res) => {
+  User.findById(req.params.userId).then((user) => {
+    const property = user.properties.id(req.params.id)
+
+    res.render('property/show', {
+      userId: req.params.userId,
+      property: property
+    })
+  })
+})
 
 // // edit page
 // router.get('/:id/edit', (req, res) => {
